ff9d46f741b921f4d21d42b2d18ca0cce6aeb87a
Laurent Pichon <tech@lescachotieres.com>
2020-06-28 16:47:05 +0200

    Pages: conference list and show on conference with comments

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,34 +2,70 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ConferenceController extends AbstractController
 {
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/", name="homepage")
-     * @param Request $request
+     * @param ConferenceRepository $conferenceRepository
      * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
-    public function index(Request $request)
+    public function index(ConferenceRepository $conferenceRepository)
     {
-        $greet = '';
-
-        if ($name = $request->query->get('hello')) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
         return new Response(
-            "
-        <html>
-            <body>
-                $greet
-                <img src='/images/under-construction.gif' />
-            </body>
-        </html>
-        "
+            $this->twig->render(
+                'conference/index.html.twig', [
+                    'conferences' => $conferenceRepository->findAll(),
+                ]
+            )
+        );
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     * @param Request $request
+     * @param Conference $conference
+     * @param CommentRepository $commentRepository
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return new Response(
+            $this->twig->render(
+                'conference/show.html.twig', [
+                    'conference' => $conference,
+                    'comments' => $paginator,
+                    'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                    'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
+                ]
+            )
         );
     }
 }
