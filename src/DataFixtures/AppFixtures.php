c1a52e9595d29b5ea3ff952fcbc5bb706f0a032e
Laurent Pichon <tech@lescachotieres.com>
2020-07-12 16:07:02 +0200

    feat: Configure fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Admin;
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoderFactory;
+
+    public function __construct(EncoderFactoryInterface $encoderFactory)
+    {
+        $this->encoderFactory = $encoderFactory;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $paris = new Conference();
+        $paris
+            ->setCity('Paris')
+            ->setYear('2019')
+            ->setIsInternational(true)
+        ;
+        $manager->persist($paris);
+
+        $lille = new Conference();
+        $lille
+            ->setCity('Lille')
+            ->setYear('2020')
+            ->setIsInternational(false)
+        ;
+        $manager->persist($lille);
+
+        $comment1 = new Comment();
+        $comment1
+            ->setConference($paris)
+            ->setAuthor('Laurent')
+            ->setEmail('contact@devlawper.com')
+            ->setText('This was a great conference.')
+        ;
+        $manager->persist($comment1);
+
+        $admin = new Admin();
+        $admin
+            ->setRoles(['ROLE_ADMIN'])
+            ->setUsername('admin')
+            ->setPassword($this->encoderFactory->getEncoder(Admin::class)->encodePassword('admin', null))
+        ;
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
